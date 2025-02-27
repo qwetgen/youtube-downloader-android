1c6282cf074f2f0475e222dd59733dfea3b7ba3f
qwetgen <kocherha1@gmail.com>
2025-02-28 00:16:12 +0200

    Fix missing Material Components dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,7 @@ android {
 }
 
 dependencies {
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.core:core-ktx:1.12.0")  // Обновляем с 1.8.0
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.compose.ui:ui:1.5.1")  // Обновляем с 1.5.0
